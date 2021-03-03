7978ccb270555053584cad5fd71feb936d7d440a
abhishek <abhishek.solanki1110@gmail.com>
2021-03-03 22:17:23 +0530

    mobile friendly changes

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default class NavBar extends React.Component{
-    constructor(props){
+let styles={};
+export default class NavBar extends React.Component {
+    constructor(props) {
         super(props)
+        this.state = {
+            onClickNav: "navbar navbar-items",
+            clicked: false
+        }
+        this.openNav = this.openNav.bind(this)
     }
-    render(){
-        return (
-            <div className="navbar navbar-items">
-                <ul>
-                    <li><Link className="link" to="/books">Books</Link></li>
-                    <li className="request"> <span>Request</span>
-                        <div className="request-dropdown">
-                            <Link className="link" to="#"><span>All Requests</span></Link><hr/>
-                            <span ><Link className="link" to="#">Create Request</Link></span>
-                        </div></li>
-                    <li><Link className="link" to="#">Trades</Link></li>
-                    <li><Link className="link" to="/users">Users</Link></li>
-                </ul>
-            </div>
+    openNav() {
+        if (this.state.clicked === false) {
+            //this.setState(()=>({clicked:false}))
+            const onClickNav = "navbar navbar-items navbar-items__mobile";
+            console.log("nav clicked");
+            this.setState(() => ({ onClickNav, clicked: true }))
+            styles = {
+                containerStyle: {
+                  left: '-0vw'
+                }
+              };
+        } else {
+            const onClickNav = "navbar navbar-items ";
+            console.log("nav un clicked");
+            this.setState(() => ({ onClickNav, clicked: false }))
+        }
 
+    }
+    render() {
+          let { containerStyle } = styles;
+        return (
+            <div>
+                <span onClick={this.openNav} className="desktop"><MenuIcon fontSize="large"/></span>
+                <div className={this.state.onClickNav} style={containerStyle}>
+                    <span onClick={this.openNav} className="cross-btn-nav desktop">X</span>
+                    <ul>
+                        <Link className="link" to="/books"><li>Books</li></Link>
+                        <li className="request"> <span>Request</span>
+                            <div className="request-dropdown">
+                                <Link className="link" to="#"><span>All Requests</span></Link><hr />
+                                <span ><Link className="link" to="#">Create Request</Link></span>
+                            </div></li>
+                        <Link className="link" to="#"><li>Trades</li></Link>
+                        <Link className="link" to="/users"><li>Users</li></Link>
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
